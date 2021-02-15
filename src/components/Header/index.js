a59580ad3c28c75c2af5e8143b4d1266a7ebbee5
Luiz André Carvalho <luizandre.ita@gmail.com>
2021-02-15 08:43:08 -0300

    header add

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,12 +1,13 @@
 import styled from 'styled-components';
 import { connect } from 'react-redux';
 import { filterByValue, sortByAlphabet, sortByAge } from "../../store";
-
+import InputGroup from 'react-bootstrap/InputGroup'
+import DropdownButton from 'react-bootstrap/DropdownButton'
+import Dropdown from 'react-bootstrap/Dropdown'
+import FormControl from 'react-bootstrap/FormControl'
 
 export const HeaderFilter = styled.header`
-    display: flex;
-    align-itens: center;
-    justify-content: space-between;
+    margin-top: 20px;
 `;
 
 const Header = (props) => {
@@ -16,7 +17,7 @@ const Header = (props) => {
     }
 
     const sortByInput = (e) => {
-        let value = e.target.value;
+        const value = e;
         let direction = value.endsWith('asc') ? "asc" : "desc";
 
         if (value.startsWith('age')) {
@@ -27,38 +28,24 @@ const Header = (props) => {
     }
     return (
         <HeaderFilter {...props}>
-            <div>
-                <section className='section'>
-                    <div className='container'>
-                        <div>
-                            <div className="field is-grouped" style={{ alignItems: "center" }}>
-                                <div className="control">
-                                    <div className="select">
-                                        <select defaultValue="" onChange={e => {
-                                            sortByInput(e)
-                                        }}>
-                                            <option value="" disabled>Sort by</option>
-
-                                            <option value='alphabet_asc'>Name - A-Z</option>
-                                            <option value='alphabet_desc'>Name - Z-A</option>
-
-                                            <option value='age_asc'>Idade - Crescente</option>
-                                            <option value='age_desc'>Idade - Decrescente</option>
-
-                                        </select>
-                                    </div>
-                                </div>
-
-                                <div className='control' style={{ minWidth: "300px" }}>
-                                    <input onChange={e => {
-                                        filterByInput(e);
-                                    }} style={{ width: "100%" }} placeholder='Filter by' type='text' />
-                                </div>
-                            </div>
-                        </div>
-                    </div >
-                </section >
-            </div >
+            <InputGroup className="mb-3">
+                <DropdownButton
+                    as={InputGroup.Prepend}
+                    variant="outline-secondary"
+                    title="Filtros"
+                    onSelect={e => sortByInput(e)}
+                    id="input-group-dropdown-1"
+                >
+                    <Dropdown.Item eventKey='alphabet_asc'>Name - A-Z</Dropdown.Item>
+                    <Dropdown.Item eventKey='alphabet_desc'>Name - Z-A</Dropdown.Item>
+                    <Dropdown.Divider />
+                    <Dropdown.Item eventKey='age_asc'>Idade - Crescente</Dropdown.Item>
+                    <Dropdown.Item eventKey='age_desc'>Idade - Decrescente</Dropdown.Item>
+                </DropdownButton>
+                <FormControl className="mb-9" onChange={e => {
+                    filterByInput(e);
+                }} aria-describedby="basic-addon1" />
+            </InputGroup>
         </HeaderFilter>
     )
 }
